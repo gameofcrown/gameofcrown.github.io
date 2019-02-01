ffb2ecce8dca059bdef64d2322a671814f81bb2f
gameofcrown <45508610+gameofcrown@users.noreply.github.com>
2019-02-02 00:27:43 +0800

    pussylicker

---
+++ js/scatter_connecter.js
@@ -0,0 +1,58 @@
+
+ScatterJS.plugins(new ScatterEOS());
+
+
+//const network = {
+//    blockchain: 'eos',
+//    chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
+//    host: '192.168.159.129',
+//    port: 7777,
+//    protocol: 'http'
+//};
+//
+
+const network = {
+    blockchain: 'eos',
+    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+    host: 'nodes.get-scatter.com',
+    port: 443,
+    protocol: 'https'
+};
+
+
+
+
+let scatter, eos;
+
+ScatterJS.scatter.connect('LernaVanillaTest').then(connected => {
+    if (!connected) return false;
+    scatter = ScatterJS.scatter;
+    eos = scatter.eos(network, Eos);
+
+
+    scatter.getIdentity({ accounts: [network] }).then(() => {
+
+        const account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
+        
+        GLOBAL_DATA.username=account.name;
+        Set_User_Info_Text("02",account.name);
+
+
+    }
+    );
+
+});
+
+window.login = async () => {
+    await scatter.suggestNetwork(network);
+    await scatter.getIdentity({ accounts: [network] })
+    eos = scatter.eos(network, Eos);
+
+
+
+};
+
+window.logout = () => {
+    scatter.forgetIdentity();
+};
+login();
---
+++ js/update_main_info.js
@@ -0,0 +1,43 @@
+function _GLOBAL_DATA()
+{
+    this.username="";
+    this.attacking_planet=0;
+    this.planet_list=null;
+    this.trading_planet=null;
+    this.notice_list=null;
+    this.notice_id=-1;
+    this.selected_planet=0;
+    this.selected_planet_info=null;
+}
+
+var GLOBAL_DATA= new _GLOBAL_DATA();
+
+function Set_User_Info_Text(index,text)
+{
+    var ele=document.getElementById("USERINFOTEXT"+index);
+    ele.innerText=text;
+}
+function ID2XY(id)
+{
+    var x,y;
+    x=parseInt(id/65536);
+    y=parseInt(id%65536);
+    x=x-32768;
+    y=y-32768;
+
+    return x+":"+y;
+
+}
+function XY2ID(str)
+{
+var a=str.split(':');
+var x,y;
+x=parseInt(a[0]);
+y=parseInt(a[1]);
+    var res;
+    res=x+32768;
+    res%=65536;
+    res*=65536;
+    res+=(y+32768)%65536;
+return res;
+}
